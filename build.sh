f37985b6ae67274019d6adeaaf16937c68c0a8ed
benjamintrapani <ben.trapani1995@gmail.com>
2020-05-23 18:30:38 -0500

    Release build type for jenkins build

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 mkdir build
 cd build
 export HIPSYCL_GPU_ARCH=sm_61
-cmake -DCMAKE_CXX_COMPILER=syclcc-clang -G Ninja ../
+cmake -DCMAKE_CXX_COMPILER=syclcc-clang -DCMAKE_BUILD_TYPE=Release -G Ninja ../
 ninja all
